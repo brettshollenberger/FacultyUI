7886043a17f810a2832c33c58226b89fa1c1c6da
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-09-17 11:00:06 -0400

    Add ngBlur directive

---
+++ directives/ngBlur/ngBlur.js
@@ -0,0 +1,12 @@
+angular
+  .module('app')
+  .directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr.ngBlur);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  };
+}]);
